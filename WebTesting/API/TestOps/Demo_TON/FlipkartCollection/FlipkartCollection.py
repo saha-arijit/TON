5d2f92fd954f8e8fa1c440cc0aed9e44295bf776
Kothalamuthu <kothalamuthuvicky@gmail.com>
2018-03-08 11:43:17 +0530

    code newman result part

---
+++ WebTesting/API/TestOps/Demo_TON/FlipkartCollection/FlipkartCollection.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import time
+
+sys.path.insert(0,'D:/TON/back_end/Web_API/')
+from Load_Thread import myThread
+thread1 = myThread(1, "Thread-1", 1)
+
+def TC_FlipkartCollection(iterations=1, vuser=1, ramp=1):
+	baseFolder="D:/TON"
+	appName   = "Demo_TON"
+	TC        = "FlipkartCollection"
+	thread1.createCSV(baseFolder,appName,TC)
+	thread1.start()
+	if(int(vuser) == 1): 
+		os.chdir("D:/TON/WebTesting/API/TestOps/Demo_TON/FlipkartCollection")
+		cmd = "newman run FlipkartCollection.json -r html --reporter-html-export newman_reports/result.html -x -n "+str(iterations)
+		os.system(cmd)
+	elif(int(vuser) > 1):
+		for i in range (int(vuser)):
+			os.chdir("D:/TON/WebTesting/API/TestOps/Demo_TON/FlipkartCollection")
+			cmd = "newman run FlipkartCollection.json -r html --reporter-html-export newman_reports/result_"+str(i)+".html -x -n "+str(iterations)
+			os.system(cmd)
+			time.sleep(int(ramp)/int(vuser))
+	thread1.stop()
---
+++ back_end/Desktop/UISpy.py
@@ -0,0 +1,14 @@
+import subprocess
+import os, time
+import sys
+
+class uiSpy:
+
+	def openSPY(self):
+		
+		# confFile = "D:/TON/bat_file/UISpy.bat"
+		# os.system ('start '+ confFile)
+		
+
+uiSpy = uiSpy()
+uiSpy.openSPY()
